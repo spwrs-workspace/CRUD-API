952e93782aacf55411838dfec2c6ec49b0afa2f3
spwrs-workspace <sspwr1502@gmail.com>
2024-05-05 16:01:57 +0530

    mounted assistant router in app

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const assistantRouter = require('./routes/assistantRoutes');
+
 const app = express();
 
+app.use('/', assistantRouter);
 module.exports = app;
