0884c9cad482478e357e216e13ab790e69fc699d
spwrs-workspace <sspwr1502@gmail.com>
2024-05-05 16:50:42 +0530

    connected to the database

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
 const port = process.env.PORT;
+
+const DB = process.env.DB_URL.replace('<password>', process.env.DB_PASSWORD);
+
+mongoose.connect(DB).then((con) => {
+  console.log('Database Connection Successful');
+});
 
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
