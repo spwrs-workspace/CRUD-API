db1165e75a8f11ce3d29a3d62830e469c2b5a4ae
spwrs-workspace <sspwr1502@gmail.com>
2024-05-06 15:29:05 +0530

    added required routes

--- routes/assistantRoutes.js
+++ routes/assistantRoutes.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const assistantController = require('../controllers/assistantController');
 
 const router = express.Router();
+
+router
+  .route('/')
+  .get(assistantController.getAllAssistants)
+  .post(assistantController.createAssistant);
+
+router
+  .route('/:id')
+  .get(assistantController.getAssistant)
+  .patch(assistantController.updateAssistant1)
+  .put(assistantController.updateAssistant2)
+  .delete(assistantController.deleteAssistant);
 
 module.exports = router;
